94f1ee1599fdf72967335fb6a9bde0f92e1e4ca2
Mohammed Muzzammil Shareef <mohdmuzzammil633@gmail.com>
2024-06-20 20:23:32 +0530

    Updated VideoBackground

--- frontend/src/components/VideoBackground.js
+++ frontend/src/components/VideoBackground.js
@@ -6,27 +6,33 @@ const VideoBackground = ({ movieId, large }) => {
   const trailerMovie = useSelector((store) => store.movie.trailerMovie);
   useMovieById(movieId);
 
-  let videoSrc;
   if (!trailerMovie || !trailerMovie.key) {
     console.log("No trailer available:", trailerMovie);
     return <div>No trailer available</div>;
   }
 
-  if (large) {
-    videoSrc = `https://www.youtube.com/embed/${trailerMovie.key}?autoplay=1&mute=1`;
-  } else {
-    videoSrc = `https://www.youtube.com/embed/${trailerMovie.key}?autoplay=1&mute=1&controls=0`;
-  }
+  const videoParams = {
+    autoplay: 1,
+    mute: 1,
+    ...(large
+      ? {}
+      : {
+          loop: 1,
+          controls: 0,
+          modestbranding: 1,
+          disablekb: 1,
+          iv_load_policy: 3,
+        }),
+  };
+
+  const paramString = new URLSearchParams(videoParams).toString();
+  const videoSrc = `https://www.youtube.com/embed/${trailerMovie.key}?${paramString}`;
 
   return (
     <div
       className={large ? "w-full h-[50vh]" : "relative w-screen aspect-video"}
     >
-      {!large && (
-        <>
-          <div className="absolute top-0 w-full h-16 bg-red z-10"></div>
-        </>
-      )}
+      {!large && <div className="absolute top-0 w-full h-16 bg-red z-10"></div>}
       <iframe
         className="w-full h-full aspect-w-16 aspect-h-9"
         src={videoSrc}
